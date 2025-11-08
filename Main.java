7e977b71509fc0d56a4a0da12f767fcb7588e581
JARPALA SIDDU <112956992+SIDDUJARPALA@users.noreply.github.com>
2025-11-08 23:55:08 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,29 @@
-package com.Arrays.MajorityOcc;
+package com.Arrays.MajorityOccers;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static List<Integer>Majority(int[]arr){
-        int n=arr.length;
-        List<Integer>li= new ArrayList<>();
-        HashMap<Integer,Integer>map =new HashMap<>();
-        int mini =(int)(n/3)+1;
+    public static int MajorityOccurs(int[] arr,int n){
+        HashMap<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<n;i++){
             int value = map.getOrDefault(arr[i],0);
             map.put(arr[i],value+1);
-            if(map.get(arr[i])==mini){
-                li.add(arr[i]);
+        }
+        for(Map.Entry<Integer,Integer>mp :map.entrySet()){
+            if(mp.getValue()>n/2){
+                return mp.getKey();
             }
-            if(li.size()==2)break;;
         }
-        return li;
-
-
-
-
+        return  -1;
     }
-    public static void main(String[]args){
-        int []arr = {11,33,33,11,33,11};
-        List<Integer>ans =Majority(arr);
-        for(int i=0;i<ans.size();i++){
-            System.out.println(ans.get(i));
-        }
-        System.out.println();
+    public static void main(String[]args)
+
+    {
+        int []arr={2, 2, 1, 1, 1, 2, 2};
+        int n=arr.length;
+        int ans = MajorityOccurs(arr,n);
+        System.out.println(ans);
+
     }
 }
